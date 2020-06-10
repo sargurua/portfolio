bbabe1beff16bb50258d1814d047b2f4f65cd39a
Avinash Sarguru <avinashsarguru@gmail.com>
2020-06-10 13:22:00 -0500

    couple of cards done, fix variable height css problem

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ class Card extends React.Component {
             <img src={this.props.img} />
             <div className="card-body">
               <h2>{this.props.title}</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+              <p>{this.props.body}</p>
               <h5>{this.props.author}</h5>
             </div>
           </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import AuthThumbnail from '../images/auth_backend.png'
+import HoopThumbnail from '../images/wehoopin-thumbnail.png'
+
 import Card from './Card'
 import Fade from 'react-reveal/Fade'
 
@@ -11,14 +14,16 @@ class Projects extends React.Component {
                     <h2 style={{color: '#4EC9B0', borderBottom: '.5vh solid #4EC9B0'}}>Projects</h2>
                     <div className='cards'>
                         <Card
-                            img='https://picsum.photos/id/54/400/300'
-                            title='What I learned from my visit to The Upside Down'
-                            author='Nancy Wheeler' 
+                            img={AuthThumbnail}
+                            title='Freelance Oauth Backend'
+                            body='Freelance project to create User login, logout, edit, sign up, and tokenization API. Made using Rails with Devise and JWT.'
+                            author='Ruby on Rails, Devise, JWT, API' 
                         />
                         <Card
-                            img='https://picsum.photos/id/54/400/300'
-                            title='What I learned from my visit to The Upside Down'
-                            author='Nancy Wheeler' 
+                            img={HoopThumbnail}
+                            title='WeHoopin'
+                            body='Personal project created while in Flatiron. NBA application that showed summarization of the previous days top performers and best games. Also used algorithmic computation to predict teams true records.'
+                            author='NodeJs, Express, React, Redux' 
                         />
                                                 <Card
                             img='https://picsum.photos/id/54/400/300'
