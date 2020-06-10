14c8c8095cdb11965f38dae7e341b117b0eb14b0
Avinash Sarguru <avinashsarguru@gmail.com>
2020-06-10 12:46:06 -0500

    card component template

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Card from './Card'
 import Fade from 'react-reveal/Fade'
 
 class Projects extends React.Component {
@@ -8,6 +9,33 @@ class Projects extends React.Component {
             <div style={{height: '100vh', backgroundColor: '#1a1a1a',}}>
                 <Fade bottom>
                     <h2 style={{color: '#4EC9B0', borderBottom: '.5vh solid #4EC9B0'}}>Projects</h2>
+                    <div className='cards'>
+                        <Card
+                            img='https://picsum.photos/id/54/400/300'
+                            title='What I learned from my visit to The Upside Down'
+                            author='Nancy Wheeler' 
+                        />
+                        <Card
+                            img='https://picsum.photos/id/54/400/300'
+                            title='What I learned from my visit to The Upside Down'
+                            author='Nancy Wheeler' 
+                        />
+                                                <Card
+                            img='https://picsum.photos/id/54/400/300'
+                            title='What I learned from my visit to The Upside Down'
+                            author='Nancy Wheeler' 
+                        />
+                                                <Card
+                            img='https://picsum.photos/id/54/400/300'
+                            title='What I learned from my visit to The Upside Down'
+                            author='Nancy Wheeler' 
+                        />
+                                                <Card
+                            img='https://picsum.photos/id/54/400/300'
+                            title='What I learned from my visit to The Upside Down'
+                            author='Nancy Wheeler' 
+                        />
+                    </div>
                 </Fade>
             </div>
         )
