d305cb4bab2b98899b08a37304a949441e690b4d
Avinash Sarguru <avinashsarguru@gmail.com>
2020-06-12 21:41:12 -0500

    minor changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import AuthThumbnail from '../images/auth_backend.png'
 import HoopThumbnail from '../images/wehoopin-thumbnail.png'
+import EventThumbnail from '../images/wegoin-thumbnail.png'
 
 import Card from './Card'
 import Fade from 'react-reveal/Fade'
@@ -22,13 +23,14 @@ class Projects extends React.Component {
                         <Card
                             img={HoopThumbnail}
                             title='WeHoopin'
-                            body='Personal project created while in Flatiron. NBA application that showed summarization of the previous days top performers and best games. Also used algorithmic computation to predict teams true records.'
+                            body='NodeJS and React Application. NBA application that showed summarization of the previous days top performers and best games. Also used algorithmic computation to predict teams true records.'
                             author='NodeJs, Express, React, Redux' 
                         />
-                                                <Card
-                            img='https://picsum.photos/id/54/400/300'
-                            title='What I learned from my visit to The Upside Down'
-                            author='Nancy Wheeler' 
+                        <Card
+                            img={EventThumbnail}
+                            title='Event Scheduler'
+                            body='Ruby on Rails application. Allowed users to create events, users could then see all events that have not passed and join them. Allowed users to friend request other users to see all their events.'
+                            author='Ruby on Rails, JWT, Devise' 
                         />
                                                 <Card
                             img='https://picsum.photos/id/54/400/300'
