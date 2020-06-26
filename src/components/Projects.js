0358be6469897e136f71443e49029fe332051212
Avinash Sarguru <avinashsarguru@gmail.com>
2020-06-25 23:07:13 -0500

    seperate modal files figured out

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import AuthModal from "./modals/AuthModal"
+import HoopModal from "./modals/HoopModal"
+
 import AuthThumbnail from '../images/thumbnails/auth_backend.png'
 import HoopThumbnail from '../images/thumbnails/wehoopin-thumbnail.png'
 import EventThumbnail from '../images/thumbnails/wegoin-thumbnail.png'
@@ -7,9 +10,6 @@ import WerewolfThumbnail from  '../images/thumbnails/werewolf-thumbnail.png'
 import TpThumbnail from '../images/thumbnails/tp-thumbnail.png'
 import ScoopThumbnail from '../images/thumbnails/scoop-thumbnail.png'
 import RandomThumbnail from '../images/thumbnails/random-thumbnail.png'
-
-import AuthError from '../images/modals/auth/postman-error-test.png'
-import AuthSignUp from '../images/modals/auth/postman-signup.png'
 
 import Card from './Card'
 import Fade from 'react-reveal/Fade'
@@ -24,6 +24,12 @@ class Projects extends React.Component {
     openAuth = () => {
         this.setState({
             authShow: true
+        })
+    }
+
+    closeAuth = () => {
+        this.setState({
+            authShow: false
         })
     }
 
@@ -81,44 +87,8 @@ class Projects extends React.Component {
                             author='Ruby on Rails, React, API, NBA API' 
                         />
                     </div>
-                    <Modal
-                        show={this.state.authShow}
-                        onHide={() => this.setState({authShow: false})}
-                        dialogClassName='modal-90w'
-                    >
-                        <Modal.Header style={{justifyContent: 'center'}}>
-                            <h1>Auth Modal</h1>
-                            <div>
-                                <img className='github-logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Font_Awesome_5_brands_github.svg/1920px-Font_Awesome_5_brands_github.svg.png' /> 
-                            </div>
-                        </Modal.Header>
-                        <Modal.Body style={{display: 'flex', justifyContent:  'center'}}><img style={{width: 'auto', height: '60vh'}} src={AuthThumbnail}></img></Modal.Body>
-                        <Modal.Body>I Worked on this Freelance project with the goal of creating an Auth Rails API Backend. Essentially the client just needed a template for User authorization set up. Using Devise and JWT I was able to set request to the backend sending either tokenization of user information or error message. </Modal.Body>
-                        <Modal.Body>Using Devise I set up the User table on Rails to provide only needed parameters such as password, username, email, and even such helper rows like password conformation. After creating the table I started creating the controllers necessary for the backend. These include the Users, Sessions, and Applications controllers. By dividing the controller I was able to organize the different routes for editing, signing up, login, etc. Setting up the params allowed in a seperate function allowed me to call the method before each action, throwing an error if the required parameters were not met.</Modal.Body>
-                        <Modal.Body style={{display: 'flex', justifyContent:  'center'}}><img style={{width: 'auto', height: '60vh'}} src={AuthError}></img></Modal.Body>
-                        <Modal.Body>Using Postman to test the API, we can see that multiple errors will be sent back from the request depending on what is wron with them. It checks for availability of email, username, and enforces password restrictions. This API also allows Users to have extra profile information for example a bio, but will not require it to be set on creation. The edit route allows users to add profile pictures and a bio post profile creation.</Modal.Body>
-                        <Modal.Body style={{display: 'flex', justifyContent:  'center'}}><img style={{width: 'auto', height: '60vh'}} src={AuthSignUp}></img></Modal.Body>
-                        <Modal.Body>On a successful request a token is recieved through JWT encryption written within the User model. It also the profile information like username and bio, but not the password. This token allows for future session storage to remember the account that is signed in without having to store the password in plain text. Though the frontend may have to encrypt the token somewhere to allow for extra security. The token is set to reset every month to allow extra security, this means Users must sign in again every month. This setting can be tweaked to never expire, but I thought the added security would be beneficial.</Modal.Body>
-                    </Modal>
-                    <Modal
-                        show={this.state.hoopShow}
-                        onHide={() => this.setState({hoopShow: false})}
-                        dialogClassName='modal-90w'
-                    >
-                        <Modal.Header style={{justifyContent: 'center'}}>
-                            <h1>Auth Modal</h1>
-                            <div>
-                                <img className='github-logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Font_Awesome_5_brands_github.svg/1920px-Font_Awesome_5_brands_github.svg.png' /> 
-                            </div>
-                        </Modal.Header>
-                        <Modal.Body style={{display: 'flex', justifyContent:  'center'}}><img style={{width: 'auto', height: '60vh'}} src={HoopThumbnail}></img></Modal.Body>
-                        <Modal.Body>Probably my personal  favorite project I have ever worked on weHoopin was the answer to the question, "How can I stay up to date on basketball while spending 14hrs a day coding during a bootcamp?".  </Modal.Body>
-                        <Modal.Body>Using Devise I set up the User table on Rails to provide only needed parameters such as password, username, email, and even such helper rows like password conformation. After creating the table I started creating the controllers necessary for the backend. These include the Users, Sessions, and Applications controllers. By dividing the controller I was able to organize the different routes for editing, signing up, login, etc. Setting up the params allowed in a seperate function allowed me to call the method before each action, throwing an error if the required parameters were not met.</Modal.Body>
-                        <Modal.Body style={{display: 'flex', justifyContent:  'center'}}><img style={{width: 'auto', height: '60vh'}} src={AuthError}></img></Modal.Body>
-                        <Modal.Body>Using Postman to test the API, we can see that multiple errors will be sent back from the request depending on what is wron with them. It checks for availability of email, username, and enforces password restrictions. This API also allows Users to have extra profile information for example a bio, but will not require it to be set on creation. The edit route allows users to add profile pictures and a bio post profile creation.</Modal.Body>
-                        <Modal.Body style={{display: 'flex', justifyContent:  'center'}}><img style={{width: 'auto', height: '60vh'}} src={AuthSignUp}></img></Modal.Body>
-                        <Modal.Body>On a successful request a token is recieved through JWT encryption written within the User model. It also the profile information like username and bio, but not the password. This token allows for future session storage to remember the account that is signed in without having to store the password in plain text. Though the frontend may have to encrypt the token somewhere to allow for extra security. The token is set to reset every month to allow extra security, this means Users must sign in again every month. This setting can be tweaked to never expire, but I thought the added security would be beneficial.</Modal.Body>
-                    </Modal>
+                    <AuthModal authShow={this.state.authShow} closeAuth={() => {this.setState({authShow: false})}}/>
+                    <HoopModal hoopShow={this.state.hoopShow} closeHoop={() => {this.setState({hoopShow: false})}}/>
                 </Fade>
             </div>
         )
--- src/components/modals/HoopModal.js
+++ src/components/modals/HoopModal.js
@@ -4,14 +4,14 @@ import Modal from 'react-bootstrap/Modal'
 
 import AuthError from '../../images/modals/auth/postman-error-test.png'
 import AuthSignUp from '../../images/modals/auth/postman-signup.png'
-import AuthThumbnail from '../../images/thumbnails/auth_backend.png'
+import HoopThumbnail from '../../images/thumbnails/wehoopin-thumbnail.png'
 
 class AuthModal extends React.Component {
-    render () {
-        return(
+    render() {
+        return (
             <Modal
-                show={this.props.authShow}
-                onHide={() => this.props.closeAuth()}
+                show={this.props.hoopShow}
+                onHide={() => this.props.closeHoop()}
                 dialogClassName='modal-90w'
             >
                 <Modal.Header style={{ justifyContent: 'center' }}>
@@ -20,8 +20,8 @@ class AuthModal extends React.Component {
                         <img className='github-logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Font_Awesome_5_brands_github.svg/1920px-Font_Awesome_5_brands_github.svg.png' />
                     </div>
                 </Modal.Header>
-                <Modal.Body style={{ display: 'flex', justifyContent: 'center' }}><img style={{ width: 'auto', height: '60vh' }} src={AuthThumbnail}></img></Modal.Body>
-                <Modal.Body>I Worked on this Freelance project with the goal of creating an Auth Rails API Backend. Essentially the client just needed a template for User authorization set up. Using Devise and JWT I was able to set request to the backend sending either tokenization of user information or error message. </Modal.Body>
+                <Modal.Body style={{ display: 'flex', justifyContent: 'center' }}><img style={{ width: 'auto', height: '60vh' }} src={HoopThumbnail}></img></Modal.Body>
+                <Modal.Body>Probably my personal  favorite project I have ever worked on weHoopin was the answer to the question, "How can I stay up to date on basketball while spending 14hrs a day coding during a bootcamp?".  </Modal.Body>
                 <Modal.Body>Using Devise I set up the User table on Rails to provide only needed parameters such as password, username, email, and even such helper rows like password conformation. After creating the table I started creating the controllers necessary for the backend. These include the Users, Sessions, and Applications controllers. By dividing the controller I was able to organize the different routes for editing, signing up, login, etc. Setting up the params allowed in a seperate function allowed me to call the method before each action, throwing an error if the required parameters were not met.</Modal.Body>
                 <Modal.Body style={{ display: 'flex', justifyContent: 'center' }}><img style={{ width: 'auto', height: '60vh' }} src={AuthError}></img></Modal.Body>
                 <Modal.Body>Using Postman to test the API, we can see that multiple errors will be sent back from the request depending on what is wron with them. It checks for availability of email, username, and enforces password restrictions. This API also allows Users to have extra profile information for example a bio, but will not require it to be set on creation. The edit route allows users to add profile pictures and a bio post profile creation.</Modal.Body>
